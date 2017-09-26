8549bdf42a9833afe494fe05cd068ddc0db1ddb3
shinnagaoka <shin.nagaoka45@gmail.com>
2017-09-26 11:34:21 +0800

    [WIP]:toppage&微調整

---
+++ toppage/example.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>例</title>
+</head>
+<body>
+	<div align="center">
+		<img src="img/example1.png" width="1000" height="550"><br><br><br>
+		<img src="img/example2.png" width="1000" height="550"><br><br><br>
+		<img src="img/example3.png" width="1000" height="550"><br><br><br>
+		<button onclick="history.back()" style="WIDTH:100px; HEIGHT:70px;">戻る</button>
+	</div>
+	<br><br><br><br><br><br>
+</body>
+</html>
